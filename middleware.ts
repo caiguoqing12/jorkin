fbba69a3e471adde3058c0b65fb09401fe0ca1f1
caiguoqing12 <guoqing.cai@ahalab.ai>
2025-08-07 16:17:19 +0800

    fix: 注册页面不重置

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname !== "/login") {
+  if (request.nextUrl.pathname !== "/login" && request.nextUrl.pathname !== "/register") {
     const token = request.cookies.get("token")?.value;
     if (!token) {
       return NextResponse.redirect(new URL("/login", request.url));
